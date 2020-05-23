612bcc088033dcd8ac475dcecfb2ce9b27114074
A N <kakaafriz@gmail.com>
2020-05-23 21:57:14 +0700

    First commit

---
+++ EQAPO Profile Switcher/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EQAPO_Profile_Switcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.disableAll = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.disableAllProfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshConfigsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // disableAll
+            // 
+            this.disableAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.disableAll.Location = new System.Drawing.Point(20, 10);
+            this.disableAll.Name = "disableAll";
+            this.disableAll.Size = new System.Drawing.Size(200, 200);
+            this.disableAll.TabIndex = 0;
+            this.disableAll.Text = "Disable All";
+            this.disableAll.UseVisualStyleBackColor = true;
+            this.disableAll.Click += new System.EventHandler(this.disableAll_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(689, 0);
+            this.vScrollBar1.Maximum = 9;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(37, 261);
+            this.vScrollBar1.SmallChange = 2;
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.disableAll);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 261);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.disableAllProfilesToolStripMenuItem,
+            this.refreshConfigsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(172, 48);
+            // 
+            // disableAllProfilesToolStripMenuItem
+            // 
+            this.disableAllProfilesToolStripMenuItem.Name = "disableAllProfilesToolStripMenuItem";
+            this.disableAllProfilesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.disableAllProfilesToolStripMenuItem.Text = "Disable All Profiles";
+            this.disableAllProfilesToolStripMenuItem.Click += new System.EventHandler(this.disableAllProfilesToolStripMenuItem_Click);
+            // 
+            // refreshConfigsToolStripMenuItem
+            // 
+            this.refreshConfigsToolStripMenuItem.Name = "refreshConfigsToolStripMenuItem";
+            this.refreshConfigsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshConfigsToolStripMenuItem.Text = "Refresh Configs";
+            this.refreshConfigsToolStripMenuItem.Click += new System.EventHandler(this.refreshConfigsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(726, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.Text = "EQAPO Profile Switcher by Afriza";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button disableAll;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem disableAllProfilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshConfigsToolStripMenuItem;
+    }
+}
+
---
+++ EQAPO Profile Switcher/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EQAPO_Profile_Switcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int scrollValue;
+        static string path = @"C:\Program Files\EqualizerAPO\config";
+
+        string[] lines = File.ReadAllLines(path + @"\config.txt");
+        bool isEnabled = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scrollValue = vScrollBar1.Value;
+            this.Width += 15;
+            refreshMethod();
+        }
+
+        private void refreshMethod()
+        {
+            bool allDisabled = false;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (!lines[i].Contains("#"))
+                {
+                    if (lines[i].Contains("Include"))
+                    {
+                        allDisabled = true;
+                    }
+                }
+            }
+            if (allDisabled)
+            {
+                disableAll.BackColor = Color.Gray;
+            }
+            else
+            {
+                disableAll.BackColor = Color.Green;
+            }
+            foreach (Button btn in panel1.Controls)
+            {
+                if (btn.Name != "disableAll")
+                {
+                    btn.Dispose();
+                }
+            }
+            string[] inactiveIncludes = new string[lines.Length];
+            int ctr = 0, top = 10, left = 222, ctrAll = 0;bool setScrollMax = true;
+            foreach (string line in lines)
+            {
+                //MessageBox.Show(ctrAll.ToString());
+                bool isActive = false;
+                if (line.Contains("Include"))
+                {
+                    ctr++;
+                    string buttonText = "";
+                    if (line.Contains("#"))
+                    {
+                        isActive = false;
+                        int inactiveIndex = line.IndexOf(":");
+                        buttonText = line.Substring(inactiveIndex + 1);
+                        //MessageBox.Show(buttonText);
+                    }
+                    else
+                    {
+                        isActive = true;
+                        int activeIndex = line.IndexOf(":");
+                        buttonText = line.Substring(activeIndex + 1);
+                        //MessageBox.Show(buttonText);
+                    }
+                    Button btn = new Button();
+                    //MessageBox.Show(ctrAll.ToString());
+                    btn.Name = (ctrAll).ToString();
+                    btn.Width = 200;
+                    btn.Height = 200;
+                    btn.Top = top;
+                    btn.Left = left;
+                    btn.Text = buttonText;
+                    btn.Anchor = (AnchorStyles.Top|AnchorStyles.Left);
+                    btn.Font = new Font(this.Font.FontFamily, 12);
+                    //btn.Click += new EventHandler(btnClick);
+
+                    btn.Click += (s, x) =>
+                    {
+                        //MessageBox.Show(btn.Name);
+                        string toSwitch = lines[Convert.ToInt32(btn.Name)];
+                        for (int i = 0; i < lines.Length; i++)
+                        {
+                            if (!lines[i].Contains("#"))
+                            {
+                                lines[i] = "#" + lines[i];
+                            }
+                        }
+                        toSwitch = toSwitch.Substring(toSwitch.IndexOf("#") + 1);
+                        //  MessageBox.Show(toSwitch);
+                        lines[Convert.ToInt32(btn.Name)] = toSwitch;
+                        if (File.Exists(path + @"\config.txt"))
+                        {
+                            File.Delete(path + @"\config.txt");
+                            File.WriteAllLines(path + @"\config.txt", lines);
+                        }
+
+                        foreach (Button buttons in panel1.Controls)
+                        {
+                            buttons.BackColor = Color.Gray;
+                            if (buttons.Name == btn.Name)
+                            {
+                                buttons.BackColor = Color.Green;
+                            }
+                        }
+                    };
+                    if (isActive)
+                    {
+                        btn.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Gray;
+                    }
+
+                    panel1.Controls.Add(btn);
+                    left += 202;
+                    if (left >= 626)
+                    {
+                        //MessageBox.Show(ctr+"");
+                        if (ctr>1)
+                        {
+                            if (setScrollMax)
+                            {
+                                vScrollBar1.Maximum = 10;
+                                setScrollMax = false;
+                            }
+                            vScrollBar1.Maximum += 1;
+                            //MessageBox.Show(vScrollBar1.Maximum.ToString());
+                        }
+                        left = 20;
+                        top += 202;
+                        panel1.Height += 202;
+                        //MessageBox.Show(left + "");
+                    }
+                }
+                ctrAll++;
+                //MessageBox.Show(ctrAll.ToString());
+            }
+        }
+
+        private void disableAll_Click(object sender, EventArgs e)
+        {
+            disableAllProfiles();
+        }
+
+        private void disableAllProfiles()
+        {
+            isEnabled = true;
+            if (isEnabled)
+            {
+                disableAll.BackColor = Color.Green;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (!lines[i].Contains("#"))
+                    {
+                        if (lines[i].Contains("Include"))
+                        {
+                            lines[i] = "#" + lines[i];
+                            //MessageBox.Show(lines[i]);
+                        }
+                    }
+                }
+                foreach (Button btn in panel1.Controls)
+                {
+                    if (btn.Name != "disableAll")
+                    {
+                        btn.BackColor = Color.Gray;
+                    }
+                }
+                if (File.Exists(path + @"\config.txt"))
+                {
+                    File.Delete(path + @"\config.txt");
+                    File.WriteAllLines(path + @"\config.txt", lines);
+                }
+            }
+            else
+            {
+                disableAll.BackColor = Color.Gray;
+            }
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int scrollValueChanged = vScrollBar1.Value;
+            if (scrollValueChanged>scrollValue)
+            {
+                panel1.Top -= 200;
+                scrollValue = scrollValueChanged;
+            }
+            else if (scrollValueChanged<scrollValue)
+            {
+                panel1.Top += 200;
+                scrollValue = scrollValueChanged;
+            }
+        }
+
+        private void disableAllProfilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            disableAllProfiles();
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button is MouseButtons.Right)
+            {
+                //contextMenuStrip1.Top = e.Y;
+                //contextMenuStrip1.Left = e.X;
+                //contextMenuStrip1.Show();
+                contextMenuStrip1.Show(Cursor.Position.X, Cursor.Position.Y);
+            }
+        }
+
+        private void refreshConfigsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            refreshMethod();
+        }
+    }
+}
